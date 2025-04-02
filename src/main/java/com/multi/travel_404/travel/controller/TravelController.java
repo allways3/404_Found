cdee011616bd1f0eec5da2a304f13dcf9271d47f
60 <129582711+allways3@users.noreply.github.com>
2025-04-02 18:51:50 +0900

    Delete src directory

--- src/main/java/com/multi/travel_404/travel/controller/TravelController.java
+++
@@ -1,4 +0,0 @@
-package com.multi.travel_404.travel.controller;
-
-public class TravelController {
-}
--- src/main/java/com/multi/travel_404/travel/model/dao/TravelMapper.java
+++
@@ -1,7 +0,0 @@
-package com.multi.travel_404.travel.model.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TravelMapper {
-}
