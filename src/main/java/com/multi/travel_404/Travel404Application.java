cd7d41507266b23d7fac3de8f17313386dc4b0e5
60 <129582711+allways3@users.noreply.github.com>
2025-04-02 12:57:25 +0900

    Add files via upload
    로그인, 공지사항, 관광지 목록, 관광지 검색, 주변 관광지 취합완료
    주변 관광지 테스트 전
    카카오맵으로 관광지 위치표시 취합중
    관광지 상세정보 추가 전

---
+++ src/main/java/com/multi/travel_404/Travel404Application.java
@@ -0,0 +1,13 @@
+package com.multi.travel_404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Travel404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Travel404Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/multi/travel_404/travel/service/TravelService.java
@@ -0,0 +1,43 @@
+package com.multi.travel_404.travel.service;
+
+import com.multi.travel_404.travel.model.dao.TravelMapper;
+import com.multi.travel_404.travel.model.dto.TravelDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class TravelService {
+    private final TravelMapper travelMapper;
+
+    private static final int PAGE_SIZE = 10;  // 한 페이지당 10개씩 - 정승호
+
+    public TravelService(TravelMapper travelMapper) {
+        this.travelMapper = travelMapper;
+    }
+
+    // 한세빈
+    public List<TravelDTO> searchTravels(String keyword) {
+        return travelMapper.searchTravels(keyword);
+    }
+
+    // 정승호
+    public List<TravelDTO> getTravelList(int page) {
+        int PAGE_SIZE = 10;
+        int offset = (page - 1) * PAGE_SIZE;  // page 1: offset=0, page 2: offset=10, ...
+        return travelMapper.getAllTravels(offset, PAGE_SIZE);
+    }
+
+    // 정승호
+    public int getTotalPages() {
+        int totalCount = travelMapper.getTotalCount();
+        return (int) Math.ceil(totalCount / (double) PAGE_SIZE);
+    }
+
+    // 안태희
+//    public List<TravelDTO> findByName(String name) {
+//        return travelMapper.findByName(name);
+//    }
+}
