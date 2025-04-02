5d7a0bc9af2546fbd67e2c139c512eddea95e921
60 <129582711+allways3@users.noreply.github.com>
2025-04-02 11:13:23 +0900

    + 김민호 취합 완료, 테스트 필요
    신유경, 한세빈, 김민호 취합 완료
    주변 관광지 검색 기능 테스트 필요

--- src/main/java/com/multi/travel_404/travel/controller/TravelController.java
+++ src/main/java/com/multi/travel_404/travel/controller/TravelController.java
@@ -5,6 +5,7 @@ import com.multi.travel_404.travel.service.TravelService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,7 +25,7 @@ public class TravelController {
         return "index";
     }
 
-    // 한세빈
+    // 관광지 검색하기 - 한세빈
     @GetMapping("/search")
     public String searchTravels(@RequestParam("keyword") String keyword, Model model) {
 
@@ -32,5 +33,12 @@ public class TravelController {
         model.addAttribute("travels", travels);
 
         return "travel/search";
+    }
+
+    // 주변 관광지 보기 - 김민호
+    @GetMapping("/travels/{title}")
+    public String getNearbyTravels(@PathVariable String title, Model model) {
+        model.addAttribute("title", title);
+        return "travel/nearbyTravels"; // nearbyTravels.html 뷰를 반환
     }
 }
